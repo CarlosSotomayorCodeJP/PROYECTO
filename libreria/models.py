1ef4f150caffb2f2d14f441bbd2e8aaaaf958809
CarlosSotomayorCodeJP <carlosmanuelsotomayorbeltran@gmail.com>
2024-09-25 01:14:26 -0400

    Base de Datos y models

--- libreria/models.py
+++ libreria/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Libro(models.Model):
+    id=models.AutoField(primary_key=True)
+    titulo=models.CharField(max_length=255, verbose_name='Título')
+    imagen=models.ImageField(upload_to='imagenes/', verbose_name='Imagen', null=True)
+    descrip=models.TextField(null=True, verbose_name='Descripción')
+
