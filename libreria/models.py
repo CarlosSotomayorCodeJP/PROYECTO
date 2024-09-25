47c14173987f216dbaa7f658d3c4042bd036c182
CarlosSotomayorCodeJP <carlosmanuelsotomayorbeltran@gmail.com>
2024-09-25 01:38:42 -0400

    Cambio a Mysql

--- libreria/models.py
+++ libreria/models.py
@@ -5,5 +5,5 @@ class Libro(models.Model):
     id=models.AutoField(primary_key=True)
     titulo=models.CharField(max_length=255, verbose_name='Título')
     imagen=models.ImageField(upload_to='imagenes/', verbose_name='Imagen', null=True)
-    descrip=models.TextField(null=True, verbose_name='Descripción')
+    descripcion=models.TextField(null=True, verbose_name='Descripción')
 
--- project_core/settings.py
+++ project_core/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'project_core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db_PROYECTO.sqlite3',
+    }
+} """
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'db_PROYECTO',
+        'USER':'root', 
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
